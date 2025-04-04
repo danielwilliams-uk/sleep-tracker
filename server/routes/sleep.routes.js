1cd91600439c24d222628478393ecb438d9cfe04
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-04 02:34:33 +0100

    update sleep router

--- server/routes/sleep.routes.js
+++ server/routes/sleep.routes.js
@@ -1,15 +1,13 @@
 import express from "express";
 import authCtrl from "../controllers/auth.controller";
-import {
-  addSleepData,
-  getUserSleepData,
-} from "../controllers/sleep.controller";
+import sleepCtrl from "../controllers/sleep.controller";
 
 const router = express.Router();
 
 // Protect routes with auth middleware
 router
-  .post("/add", authCtrl, addSleepData)
-  .get("/", authCtrl, getUserSleepData);
+  .route("/api/sleep")
+  .post(authCtrl.requireSignin, sleepCtrl.addSleepData)
+  .get(authCtrl.requireSignin, sleepCtrl.getUserSleepData);
 
 export default router;
