b5f634866c2a1add80911a6c9c327dcad3df65b6
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-05 16:16:42 +0100

    feat: update App to remove server-side injected CSS

--- client/App.js
+++ client/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainRouter from "./MainRouter";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
@@ -6,6 +6,12 @@ import { hot } from "react-hot-loader";
 import theme from "./theme";
 
 const App = () => {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
