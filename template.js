0370ce660028f1a50b0455fcec60fb73673cf68d
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-05 16:09:01 +0100

    feat: update template to load server-generatedcode in browser

--- template.js
+++ template.js
@@ -1,14 +1,20 @@
-export default () => {
+export default ({ markup, css }) => {
   return `<!doctype html>
         <html lang="en">
             <head>
                 <meta charset="utf-8">
+                <title>Sleep tracker</title>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
                 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-                <title>Sleep tracker</title>
+                <style>
+                    a {
+                        text-decoration: none;
+                        color:rgb(0, 98, 255)
+                    }
             </head>
-            <body>
-                <div id="root"></div>
+            <body style="margin:0">
+                <div id="root">${markup}</div>
+                <style id="jss-server-side">${css}</style>
                 <script type="text/javascript" src="/dist/bundle.js"></script>
             </body>
         </html>`;
