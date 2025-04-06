394529dbdbafcd8e0536830374c9c8fbc4aa9aaa
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-06 20:49:12 +0100

    feat: update webpack to use modern js syntax for new date picker

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -1,44 +1,49 @@
-const path = require('path')
-const webpack = require('webpack')
-const CURRENT_WORKING_DIR = process.cwd()
+const path = require("path");
+const webpack = require("webpack");
+const CURRENT_WORKING_DIR = process.cwd();
 
 const config = {
-    name: "browser",
-    mode: "development",
-    devtool: 'eval-source-map',
-    entry: [
-        'webpack-hot-middleware/client?reload=true',
-        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+  name: "browser",
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: [
+    "webpack-hot-middleware/client?reload=true",
+    path.join(CURRENT_WORKING_DIR, "client/main.js"),
+  ],
+  output: {
+    path: path.join(CURRENT_WORKING_DIR, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules\/(?!(@mui|@babel)\/).*/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-proposal-optional-chaining",
+              "@babel/plugin-proposal-nullish-coalescing-operator",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+        use: "file-loader",
+      },
     ],
-    output: {
-        path: path.join(CURRENT_WORKING_DIR , '/dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader'
-                ]
-            },
-            {
-                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
-                use: 'file-loader'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
-    ],
-    resolve: {
-        alias: {
-            'react-dom': '@hot-loader/react-dom'
-        }
-    }
-}
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  resolve: {
+    alias: {},
+  },
+};
 
-module.exports = config
+module.exports = config;
--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -1,31 +1,36 @@
-const path = require('path')
-const CURRENT_WORKING_DIR = process.cwd()
+const path = require("path");
+const CURRENT_WORKING_DIR = process.cwd();
 
 const config = {
-    mode: "production",
-    entry: [
-        path.join(CURRENT_WORKING_DIR, 'client/main.js')
+  mode: "production",
+  entry: [path.join(CURRENT_WORKING_DIR, "client/main.js")],
+  output: {
+    path: path.join(CURRENT_WORKING_DIR, "/dist"),
+    filename: "bundle.js",
+    publicPath: "/dist/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules(?!\/(@mui|@babel)\/)/, // Include @mui and @babel for transpilation
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              "@babel/plugin-proposal-optional-chaining",
+              "@babel/plugin-proposal-nullish-coalescing-operator",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+        use: "file-loader",
+      },
     ],
-    output: {
-        path: path.join(CURRENT_WORKING_DIR , '/dist'),
-        filename: 'bundle.js',
-        publicPath: "/dist/"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    'babel-loader'
-                ]
-            },
-            {
-                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
-                use: 'file-loader'
-            }
-        ]
-    }
-}
+  },
+};
 
-module.exports = config
+module.exports = config;
