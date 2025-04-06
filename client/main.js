15033a618a5c6c75b763e4325293b180d9469c5b
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-06 15:15:46 +0100

    feat: add root element to DOM if not added

--- client/main.js
+++ client/main.js
@@ -2,4 +2,14 @@ import React from "react";
 import { hydrate } from "react-dom";
 import App from "./App";
 
-hydrate(<App />, document.getElementById("root"));
+document.addEventListener("DOMContentLoaded", () => {
+  let rootElement = document.getElementById("root");
+  if (!rootElement) {
+    console.error("Root element not found! Creating one dynamically.");
+    rootElement = document.createElement("div");
+    rootElement.id = "root";
+    document.body.appendChild(rootElement);
+  }
+
+  hydrate(<App />, rootElement);
+});
