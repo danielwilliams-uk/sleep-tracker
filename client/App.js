f2400bab7311be4d0f332a6e98291d4010292609
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-06 12:04:01 +0100

    feat: update useEffect hook

--- client/App.js
+++ client/App.js
@@ -6,7 +6,7 @@ import { hot } from "react-hot-loader";
 import theme from "./theme";
 
 const App = () => {
-  React.useEffect(() => {
+  useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
