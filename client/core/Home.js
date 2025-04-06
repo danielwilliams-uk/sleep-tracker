04629fe70f1d4880d7570e5147d01941610e8d35
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-06 15:14:25 +0100

    feat: update home page

--- client/core/Home.js
+++ client/core/Home.js
@@ -2,9 +2,12 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import caterpillarImg from "./../assets/images/caterpillar.jpg";
+import cat from "./../assets/images/cat-sleeping.jpg";
+import CardMedia from "@material-ui/core/CardMedia";
+import auth from "../auth/auth-helper";
+import { Link } from "react-router-dom";
+// import SleepDataverview from "../sleepdata/SleepDataOverview";
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -12,6 +15,7 @@ const useStyles = makeStyles((theme) => ({
     margin: "auto",
     marginTop: theme.spacing(5),
     marginBottom: theme.spacing(5),
+    // backgroundColor: theme.palette.primary.dark,
   },
   title: {
     padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(
@@ -36,22 +40,24 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
   const classes = useStyles();
   return (
-    <Card className={classes.card}>
-      <Typography variant="h6" className={classes.title}>
-        Home Page
-      </Typography>
-      <CardMedia
-        className={classes.media}
-        image={caterpillarImg}
-        title="Caterpillar"
-        alt="Caterpillar"
-      />
-      <CardContent>
-        <Typography variant="body1" component="p">
-          The caterpillar plays a vital role in nature as it transforms into a
-          butterfly, contributing to pollination and biodiversity.
-        </Typography>
-      </CardContent>
-    </Card>
+    <>
+      {/* {auth.isAuthenticated() && <SleepDataOverview />} */}
+      {!auth.isAuthenticated() && typeof window !== "undefined" && (
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.media}
+            image={cat}
+            title="Unicorn Bicycle"
+          />
+          <CardContent style={{ textAlign: "center" }}>
+            <Typography variant="body1" component="p">
+              Welcome to Sleep Tracker. <Link to="/signup">Sign up</Link> to
+              start your journey to better sleep today or{" "}
+              <Link to="/signin">Log In</Link> to track your sleep pattern .
+            </Typography>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 }
