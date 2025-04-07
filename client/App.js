a6ba07435cdf37a0cdb3582fe2042e7a1dc12ac6
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-07 01:19:50 +0100

    feat: remove hot module reloading

--- client/App.js
+++ client/App.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import MainRouter from "./MainRouter";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@material-ui/core/styles";
-import { hot } from "react-hot-loader";
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
 
 const App = () => {
@@ -21,4 +20,4 @@ const App = () => {
   );
 };
 
-export default hot(module)(App);
+export default App;
