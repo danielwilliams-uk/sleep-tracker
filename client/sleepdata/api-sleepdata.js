907102242e925460fc9f755324c6149bf79aef08
danielwilliams-uk <daniel_._williams@outlook.com>
2025-04-19 02:17:16 +0100

    chore: remove getUserSleepData and add logs for debugging

--- client/sleepdata/api-sleepdata.js
+++ client/sleepdata/api-sleepdata.js
@@ -1,8 +1,9 @@
 import qs from "qs";
+import queryString from "query-string";
 
 const create = async (credentials, sleepdata) => {
   try {
-    let response = await fetch("/api/sleepdata", {
+    let response = await fetch("/api/sleepdata/", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -18,39 +19,24 @@ const create = async (credentials, sleepdata) => {
   }
 };
 
-const getUserSleepData = async (params, credentials, signal) => {
-  try {
-    let response = await fetch("/api/sleepdata/" + params.sleepdataId, {
-      method: "GET",
-      signal: signal,
-      headers: {
-        Accept: "application/json",
-        Authorization:
-          credentials && credentials.t ? "Bearer " + credentials.t : undefined,
-      },
-    });
-    return await response.json();
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const listByUser = async (params, credentials, signal) => {
-  const query = qs.stringify(params);
+  const query = queryString.stringify(params);
+  console.log("===================>>> params inside listByUser:", params);
   try {
+    console.log("===================>>> queryString inside try block:", query);
     let response = await fetch("/api/sleepdata?" + query, {
       method: "GET",
       signal: signal,
       headers: {
         Accept: "application/json",
-        Authorization:
-          credentials && credentials.t ? "Bearer " + credentials.t : undefined,
+        Authorization: "Bearer " + credentials.t,
       },
     });
+    console.log("=====================>>> Response from fetch:", response);
+
     return await response.json();
   } catch (err) {
     console.log(err);
   }
 };
-
-export { create, getUserSleepData, listByUser };
+export { create, listByUser };
